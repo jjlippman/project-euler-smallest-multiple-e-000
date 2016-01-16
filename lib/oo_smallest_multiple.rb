b44af20971655b8ea418622615b8685ed4be7aac
jjlippman <jjlippman@earthlink.net>
2016-01-16 15:27:04 -0700

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,22 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+
+  def initialize(num)
+    @num=num
+  end
+
+  def lcm
+    org = @num
+    reset=@num
+    while @num > 1 do
+      if org % @num == 0
+        @num-=1
+      else
+        org += reset
+        @num=reset
+      end
+     end
+    org
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def smallest_multiple(i)
+   org = i
+   reset=i
+  while i > 1 do
+    if org % i == 0
+      i-=1
+    else
+      org += reset
+      i=reset
+    end
+  end
+ org
+end
+
+
+
+           
